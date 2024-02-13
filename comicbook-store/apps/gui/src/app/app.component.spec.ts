2dbdfe9d3063f5e870111640d4ee5112410bf9c6
Tomasz Najbar <najbartomasz@gmail.com>
2024-02-13 22:59:27 +0100

    empty angular application

---
+++ comicbook-store/apps/gui/src/app/app.component.spec.ts
@@ -0,0 +1,17 @@
+import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AppComponent } from './app.component';
+
+describe('AppComponent', () => {
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            imports: [AppComponent, RouterTestingModule],
+        }).compileComponents();
+    });
+
+    it('should be created', () => {
+        const fixture = TestBed.createComponent(AppComponent);
+        fixture.detectChanges();
+        expect(fixture.componentInstance).toBeTruthy();
+    });
+});
