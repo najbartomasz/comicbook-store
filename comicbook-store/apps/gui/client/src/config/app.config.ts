63eefbe439d49c391fde769353b6b5c865796ffb
Tomasz Najbar <najbartomasz@gmail.com>
2024-04-21 17:42:11 +0200

    fixes for import paths

--- comicbook-store/apps/gui/client/src/config/app.config.ts
+++ comicbook-store/apps/gui/client/src/config/app.config.ts
@@ -2,8 +2,8 @@ import { ApplicationConfig } from '@angular/core';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideRouter } from '@angular/router';
 import { provideApi } from 'config/providers/api/api.provider';
-import { appRoutes } from '../routes/app.routes';
 import { provideFeatures } from 'config/providers/feature/feature.provider';
+import { appRoutes } from 'routes/app.routes';
 import { provideLib } from './providers/lib/lib.provider';
 
 export const appConfig: ApplicationConfig = {
