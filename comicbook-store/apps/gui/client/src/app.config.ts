4a735cc3617e2b458bd49f44d1a1b560e761ba74
Tomasz Najbar <najbartomasz@gmail.com>
2025-01-26 21:04:18 +0100

    in-memory-database removed - brandigs are taken from data-warehouse server

--- comicbook-store/apps/gui/client/src/app.config.ts
+++ comicbook-store/apps/gui/client/src/app.config.ts
@@ -1,17 +1,14 @@
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { ApplicationConfig } from '@angular/core';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { withLoggingInterceptor } from '@ui/interceptors';
-import { InMemoryDatabaseService } from '@ui/services';
-import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { appRoutes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
     providers: [
         provideRouter(appRoutes, withComponentInputBinding()),
         provideHttpClient(withInterceptors([withLoggingInterceptor()])),
-        importProvidersFrom(InMemoryWebApiModule.forRoot(InMemoryDatabaseService, { apiBase: '/' })),
         provideAnimationsAsync()
     ]
 };
--- comicbook-store/apps/gui/client/src/app/infrastructure/ui/services/index.ts
+++ comicbook-store/apps/gui/client/src/app/infrastructure/ui/services/index.ts
@@ -1,3 +1,2 @@
 export * from './dynamic-component-factory';
 export { FeatureFactoryRepositoryService } from './feature-factory-repository/feature-factory-repository.service';
-export { InMemoryDatabaseService } from './in-memory-database/in-memory-database.service';
