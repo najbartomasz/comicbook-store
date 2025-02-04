cc7574539b064453ae22145ef8aaa3fbf1e9705d
Tomasz Najbar <najbartomasz@gmail.com>
2025-02-04 11:19:33 +0100

    [TG-5] Added default nx angular application with ssr enabled.

---
+++ apps/gui/src/app/app.config.ts
@@ -0,0 +1,15 @@
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {
+    provideClientHydration,
+    withEventReplay,
+} from '@angular/platform-browser';
+import { provideRouter } from '@angular/router';
+import { appRoutes } from './app.routes';
+
+export const appConfig: ApplicationConfig = {
+    providers: [
+        provideClientHydration(withEventReplay()),
+        provideZoneChangeDetection({ eventCoalescing: true }),
+        provideRouter(appRoutes),
+    ],
+};
